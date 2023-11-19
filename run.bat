bf39da7063bf6033179ae33b3f9e665179fdb673
David M <136613722+realize-1337@users.noreply.github.com>
2023-11-19 21:15:14 +0100

    created bat to execute

---
+++ run.bat
@@ -0,0 +1,22 @@
+@echo off
+
+cd C:\Users\david\Documents\Dev\ex2bib
+ECHO Path Done!
+if %ERRORLEVEL% neq 0 goto exit
+
+call venv\Scripts\activate
+if %ERRORLEVEL% neq 0 goto createVenv
+goto runner
+
+:createVenv
+ECHO CREATING VENV
+call python -m venv venv
+call venv\Scripts\activate
+call pip install -r requirements.txt
+
+:runner
+ECHO at Runner
+venv\Scripts\python.exe ex2bib.py --david
+deactivate
+
+exit
